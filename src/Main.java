75293863c19bbd540a5ae5d7778998a1da77f2ae
nmirc <n.mircea@yahoo.com>
2022-10-04 11:23:03 +0300

    Lab1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        var ct = 0;
+        float sum = 0;
+
+        while (scanner.hasNextInt()) {
+            sum += scanner.nextInt();
+            ct++;
+        }
+
+        float average = sum / ct;
+
+        System.out.println(average);
+
+    }
+}
